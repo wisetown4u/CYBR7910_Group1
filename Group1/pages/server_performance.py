10d1fb576a0070cb1e36b6262ba1ddcfc3d26df6
wisetown4u <WLEWALLE@STUDENTS.KENNESAW.EDU>
2025-07-18 19:57:07 -0400

    Update server_performance.py

--- Group1/pages/server_performance.py
+++ Group1/pages/server_performance.py
@@ -3,16 +3,29 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 from dash import Dash, html, dcc, Input, Output, callback, dash_table
-from plotly.graph_objs import Figure
 import dash_ag_grid as dag
 
 df1 = pd.read_csv('datasets/Dataset 1__Web_Server_Access.csv')
 df2 = pd.read_csv('datasets/Dataset 4__Network_Traffic_Summary.csv')
 
-#lineIn = px.scatter(df2, x='sample_time', y='inbound_bytes', color='suspicious_activity')
+
 fig_pie = px.pie(df1, values='http_method', names='status_code', color='status_code', title='Method Status Code'),
-#activity = df2.suspicious_activity.unique().tolist()
-#lineIn.show()
+barIn = px.bar(df2, x='protocol', y='inbound_bytes', color='suspicious_activity', title= 'Inbound Suspicious Activity by Protocol')
+barOut = px.bar(df2, x='protocol', y='outbound_bytes', color='suspicious_activity', title= 'Outbound Suspicious Activity by Protocol')
+piechartIn = px.pie(
+        data_frame=df2,
+        values='inbound_bytes',
+        names='suspicious_activity',
+        title='Suspicious Inbound Bytes',
+)
+
+piechartOut = px.pie(
+         data_frame=df2,
+         values='outbound_bytes',
+         names='suspicious_activity',
+         title='Suspicious Outbound Bytes',
+)
+
 
 app = dash.Dash(__name__)
 
@@ -21,25 +34,25 @@ layout = html.Div([
     html.H1("View Server Performance"),
     dbc.Button("Home", href="/Home", color="link"),
 
-    # dcc.Dropdown(id='methodChoice', options=
-    #     [{'label':x, 'value':x}
-    #     for x in df1.http_method.unique()],
-    #         value=df1.http_method.unique(), multi=True,
+        html.Div([
+
+        dcc.Graph(figure=piechartIn, style={'width': '25%', 'display': 'inline-block'}),
+        dcc.Graph(figure=barIn, style={'width': '75%', 'display': 'inline-block'}),
+        dcc.Graph(figure=piechartOut, style={'width': '25%', 'display': 'inline-block'}),
+        dcc.Graph(figure=barOut, style={'width': '75%', 'display': 'inline-block'}),
+
+        ]),
 
         html.Div([
-            dcc.Checklist(
-                id='checklist',
+            dcc.RadioItems(
+                id='radioItem',
                 options=[{'label':x, 'value':x, 'disabled':False}
-                         for x in df2['suspicious_activity'].unique()
-                         ],
-                value=['No','Yes'],
+                for x in df1['url_accessed'].unique()],
                 inline=True,
+                value='/admin',
+                inputStyle={'margin-right': '5px', 'margin-left': '10px'},
             ),
-        dcc.Graph(id='mypieIn', figure={}, style={'display': 'inline-block'}),
-        dcc.Graph(id='mypieOut', figure={}, style={'display': 'inline-block'}),
-        dcc.Graph(id='myScatIn', figure={}, style={'display': 'inline-block'}),
-        dcc.Graph(id='myScatOut', figure={}, style={'display': 'inline-block'}),
-        dcc.Graph(id='myLineIn', figure={}, style={'display': 'inline-block'}),
+            dcc.Graph(id='status_hist', figure={})
         ]),
 
 
@@ -58,45 +71,26 @@ layout = html.Div([
 ]),
 
 @callback(
-#  Output('myHist', 'figure'),
-         Output('mypieIn', 'figure'),
-         Output('mypieOut', 'figure'),
-         Output('myScatIn', 'data'),
-         Output('myScatOut', 'data'),
-#         Input('methodChoice', 'value'),
-          Input('checklist', 'value'),
-#
+    Output('status_hist', 'figure'),
+    [Input('radioItem', 'value'),]
 )
-def update_graph(value_chosen):
-#     #print(value_method)
-    dff2 = df2[df2['suspicious_activity'].isin(value_chosen)]
+def update_graph(url_chosen):
+
+    dff = df1
+    if type(url_chosen) != str:
+        dff1 = dff[dff['url_accessed'].isin(url_chosen)]
+    else:
+        dff1 = dff[dff.url_accessed==url_chosen]
 
 
-    # if type(value_chosen) != str:
-    #     dff = df2[df2['suspicious_activity'].isin(value_chosen)]
-    # else:
-    #     dff = df2[df2.http_method == value_chosen],
 
-    piechartIn = px.pie(
-        data_frame=dff2,
-        values='inbound_bytes',
-        names='suspicious_activity',
-        title='Suspicious Inbound Bytes',
-    )
-    piechartOut = px.pie(
-         data_frame=dff2,
-         values='outbound_bytes',
-         names='suspicious_activity',
-         title='Suspicious Outbound Bytes',
-    )
-    scatIn = px.scatter(df2, x='sample_time', y='inbound_bytes', color='suspicious_activity', title= 'IN')
-    scatOut = px.scatter(df2, x='sample_time', y='outbound_bytes', color='suspicious_activity')
+    url_fig = px.scatter(dff1, x='status_code', y='response_time_ms', color='url_accessed')
+    url_fig.update_layout(title="Status Code of URLs Accessed",
+                               yaxis_title='Response Time (ms)',
+                               xaxis_title='Status Code')
+    return url_fig
 
-#
-#     fig_hist1 = px.histogram(dff, x='timestamp', y='response_time_ms', color='http_method')
-#     fig_scat1 = px.scatter(dff, x='timestamp', y='response_time_ms', color='http_method')
-#     fig_urlscat= px.scatter(dff, x='url_accessed', y='response_time_ms', color='http_method', title='URL Response Time by Method')
-#
-    return piechartIn, piechartOut, scatIn, scatOut
+
+
 
 
