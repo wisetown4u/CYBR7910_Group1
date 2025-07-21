fd2362e5dbd5f6c1be1530a2867f59fb98be8f02
wisetown4u <WLEWALLE@STUDENTS.KENNESAW.EDU>
2025-07-21 19:43:48 -0400

    Add files via upload

--- Group1/pages/request_methods.py
+++ Group1/pages/request_methods.py
@@ -3,7 +3,6 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 from dash import Dash, html, dcc, Input, Output, callback, dash_table
-from plotly.graph_objs import Figure
 import dash_ag_grid as dag
 
 df1 = pd.read_csv('datasets/Dataset 1__Web_Server_Access.csv')
@@ -22,8 +21,8 @@ layout = html.Div([
 
     ),
     html.Div([
-        dcc.Graph(id='myHist', figure={}, style={'display': 'inline-block'}),
-        dcc.Graph(id='myScat', figure={}, style={'display': 'inline-block'}),
+        dcc.Graph(id='myHist', figure={}, style={'width': '50%', 'display': 'inline-block'}),
+        dcc.Graph(id='myScat', figure={}, style={'width': '50%', 'display': 'inline-block'}),
         html.Div([
         dcc.Graph(id='urlMethod', figure={}),
         ]),
@@ -38,21 +37,25 @@ layout = html.Div([
 ])
 
 @callback(
-    Output('myHist', 'figure'),
-    Output('myScat', 'figure'),
-    Output('urlMethod', 'figure'),
-    Input('methodChoice', 'value'),
+ Output('myHist', 'figure'),
+        Output('myScat', 'figure'),
+        Output('urlMethod', 'figure'),
+        #Output('grid', 'data'),
+        Input('methodChoice', 'value'),
 
 )
 def interactive_graph(value_method):
+
     if type(value_method) != str:
        dff = df1[df1['http_method'].isin(value_method)]
     else:
         dff = df1[df1.http_method==value_method],
 
     fig_hist1 = px.histogram(dff, x='timestamp', y='response_time_ms', color='http_method')
-    fig_scat1 = px.scatter(dff, x='timestamp', y='response_time_ms', color='http_method')
-    fig_urlscat= px.scatter(dff, x='url_accessed', y='response_time_ms', color='http_method', title='URL Response Time by Method')
+    fig_scat1 = px.scatter(dff, x='timestamp', y='response_time_ms', color='http_method', hover_data=['status_code', 'ip_address'])
+    fig_urlscat = px.scatter(dff, x='url_accessed', y='response_time_ms', color='http_method', title='URL Response Time by Method',
+                             hover_data=['status_code', 'ip_address'])
+
 
     return fig_hist1, fig_scat1, fig_urlscat
 
